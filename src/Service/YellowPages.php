474bc858684f0bb4ab5ddfaab0b2f0613fb0cefb
Buster Neece <buster@busterneece.com>
2023-04-26 09:52:44 -0500

    Working proof-of-concept for YP integration.

--- src/Service/YellowPages.php
+++ src/Service/YellowPages.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Entity\Api\NowPlaying\NowPlaying;
-use App\Entity\Station;
 use App\Version;
 use GuzzleHttp\Client as GuzzleClient;
 use GuzzleHttp\Promise\PromiseInterface;
@@ -27,27 +26,26 @@ final class YellowPages
 
     public function add(
         string $endpoint,
-        Station $station
+        NowPlaying $nowPlaying
     ): PromiseInterface {
         return $this->sendRequest(
             $endpoint,
             self::ACTION_ADD,
-            [
-                'sn' => $station->getName(),
+            array_filter([
+                'sn' => $nowPlaying->station->name,
                 'type' => 'music',
-                'genre' => $station->getGenre(),
+                'genre' => $nowPlaying->station->genre,
                 'b' => '128',
-                'listenurl' => '', // TODO
-                'desc' => $station->getDescription(),
-                'url' => $station->getUrl(),
-            ]
+                'listenurl' => $nowPlaying->station->listen_url,
+                'desc' => $nowPlaying->station->description,
+                'url' => $nowPlaying->station->url,
+            ])
         );
     }
 
     public function touch(
         string $endpoint,
         string $sessionId,
-        Station $station,
         NowPlaying $nowPlaying
     ): PromiseInterface {
         return $this->sendRequest(
@@ -89,38 +87,33 @@ final class YellowPages
             $endpoint,
             [
                 RequestOptions::HEADERS => [
-                    'User-Agent' => 'AzuraCast ' . Version::FALLBACK_VERSION,
+                    'User-Agent' => 'Icecast (AzuraCast ' . Version::FALLBACK_VERSION . ' )',
                     'Accept' => '*/*',
                 ],
-                RequestOptions::BODY => $requestBody,
+                RequestOptions::FORM_PARAMS => $requestBody,
             ]
         )->then(
-            function (ResponseInterface $response) use ($requestBody) {
-                if ($response->getStatusCode() !== 200) {
-                    $this->logger->error(
-                        sprintf('Request returned status code %d.', $response->getStatusCode()),
-                        [
-                            'body' => $requestBody,
-                        ]
-                    );
-                    return false;
-                }
-
+            function (ResponseInterface $response) use ($action) {
                 $responseCode = $response->getHeaderLine('YPResponse');
                 if (empty($responseCode)) {
-                    $this->logger->error(
+                    throw new \RuntimeException(
                         sprintf(
                             'Remote server indicated failure: %s',
                             $response->getHeaderLine('YPMessage')
-                        ),
-                        [
-                            'body' => $requestBody,
-                        ]
+                        )
                     );
-                    return false;
                 }
 
-                if (self::ACTION_ADD === $requestBody['action']) {
+                if ($response->getStatusCode() !== 200) {
+                    throw new \RuntimeException(
+                        sprintf(
+                            'Request returned status code %d',
+                            $response->getStatusCode()
+                        )
+                    );
+                }
+
+                if (self::ACTION_ADD === $action) {
                     $sessionId = $response->getHeaderLine('SID');
                     return (!empty($sessionId))
                         ? $sessionId
